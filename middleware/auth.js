f360a1685a4180e148856222dbda867be8f9382e
Manish-Yadav77 <manishkyadav969@gmail.com>
2025-04-26 02:09:00 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
@@ -14,4 +14,4 @@ const authenticateToken = (req, res, next) => {
   }
 };
 
-module.exports = authenticateToken;
+export default authenticateToken;
